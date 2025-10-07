310be8e8ef0a0ae48db0bd6220e74971286decec
BerkeBoran <2022141046@cumhuriyet.edu.tr>
2025-10-07 18:43:20 +0300

    Initial Commit

---
+++ FinMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinMate
+//
+//  Created by Berke Boran on 7.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinMate/FinMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinMateApp.swift
+//  FinMate
+//
+//  Created by Berke Boran on 7.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct FinMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
