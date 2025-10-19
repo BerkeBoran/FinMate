83a0936898533a546ac146e35d3a6d4505a1c8b8
BerkeBoran <2022141046@cumhuriyet.edu.tr>
2025-10-19 21:55:32 +0300

    add payment schedule entity to core data model

---
+++ FinMate/PaymentScheduleView.swift
@@ -0,0 +1,8 @@
+//
+//  PaymentScheduleView.swift
+//  FinMate
+//
+//  Created by Berke Boran on 19.10.2025.
+//
+
+import Foundation
---
+++ ViewModels/PaymentScheduleViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PaymentScheduleViewModel.swift
+//  FinMate
+//
+//  Created by Berke Boran on 19.10.2025.
+//
+
+import Foundation
